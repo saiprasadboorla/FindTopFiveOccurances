5522880a243b43fb83d075af1818acf4b0b69b0f
Sai Prasad <sai.boorla@gmail.com>
2023-04-03 01:11:52 +0100

    initial commit

---
+++ Exercise.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Technical Exercise" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Exercise.aspx.cs" Inherits="TopFiveWebApp.Contact" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Find five most commonly occurring numbers</h2>
+     
+    <h3><asp:Label runat ="server" Text ="Please upload a text file"></asp:Label></h3>
+    <asp:FileUpload id="fileUpLoader" runat="server" />  
+    <br />
+    <asp:Button id="UploadBtn" Text="Upload File" OnClick="UploadBtn_Click" runat="server" Width="105px" />  
+    <asp:Label ID ="lblFileUploaded" runat ="server"></asp:Label>
+    <br />
+    <br />
+    <asp:Label ID ="lblResult" runat ="server"></asp:Label>
+</asp:Content>
---
+++ Exercise.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TopFiveWebApp
+{
+	public partial class Contact : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+        protected void UploadBtn_Click(object sender, EventArgs e)
+        {
+            if (fileUpLoader.HasFile)
+            {
+                fileUpLoader.SaveAs(@"C:\Sai\" + fileUpLoader.FileName);
+                lblFileUploaded.Text = "File Uploaded: " + fileUpLoader.FileName;
+            }
+            else
+            {
+                lblFileUploaded.Text = "No File Uploaded.";
+            }
+
+            if (fileUpLoader.HasFile)
+            {
+                StreamReader reader = new StreamReader(fileUpLoader.FileContent);
+                string content = reader.ReadToEnd().Trim();
+                string[] read = content.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                List<Int64> lstNumbers = read.Select(Int64.Parse).ToList();
+                var commonNumbers = lstNumbers.GroupBy(m => m).Select(g => new { Value = g.Key, Count = g.Count() }).OrderByDescending(m => m.Count).Take(5);
+                string result = string.Empty;
+                foreach (var num in commonNumbers)
+				{
+                    result += num.Value + "<br>";
+                }
+                lblResult.Text = "<b>Result: </b>"+ "<br>" + result;
+                
+            }
+
+        }
+    }
+}
