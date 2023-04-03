43029f0aa4d2c03b961a15f6af7e08991a182e6a
Sai Prasad <sai.boorla@gmail.com>
2023-04-03 03:02:41 +0100

    Modified test file validation

--- Exercise.aspx
+++ Exercise.aspx
@@ -1,10 +1,8 @@
 ﻿<%@ Page Title="Technical Exercise" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Exercise.aspx.cs" Inherits="TopFiveWebApp.Contact" %>
 
-<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2>Find five most commonly occurring numbers</h2>
-     
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">     
     <h3><asp:Label runat ="server" Text ="Please upload a text file"></asp:Label></h3>
-    <asp:FileUpload id="fileUpLoader" runat="server" />  
+    <asp:FileUpload id="fileUpLoader" runat="server"/>  
     <br />
     <asp:Button id="UploadBtn" Text="Upload File" OnClick="UploadBtn_Click" runat="server" Width="105px" />  
     <asp:Label ID ="lblFileUploaded" runat ="server"></asp:Label>
@@ -12,3 +10,5 @@
     <br />
     <asp:Label ID ="lblResult" runat ="server"></asp:Label>
 </asp:Content>
+
+
--- Exercise.aspx.cs
+++ Exercise.aspx.cs
@@ -13,33 +13,39 @@ namespace TopFiveWebApp
 		protected void Page_Load(object sender, EventArgs e)
 		{
 
-		}
+        }
         protected void UploadBtn_Click(object sender, EventArgs e)
-        {
+        {       
             if (fileUpLoader.HasFile)
             {
-                fileUpLoader.SaveAs(@"C:\Sai\" + fileUpLoader.FileName);
-                lblFileUploaded.Text = "File Uploaded: " + fileUpLoader.FileName;
+                string fileExtension = Path.GetExtension(fileUpLoader.FileName);
+
+                if (fileExtension.ToLower() != ".txt")
+                {
+                    lblFileUploaded.Text = "Only .txt file allowed";
+                    lblFileUploaded.ForeColor = System.Drawing.Color.Red;
+                }
+				else
+				{
+                    StreamReader reader = new StreamReader(fileUpLoader.FileContent);
+                    string content = reader.ReadToEnd().Trim();
+                    string[] read = content.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                    List<Int64> lstNumbers = read.Select(Int64.Parse).ToList();
+                    var commonNumbers = lstNumbers.GroupBy(m => m).Select(g => new { Value = g.Key, Count = g.Count() }).OrderByDescending(m => m.Count).Take(5);
+                    string result = string.Empty;
+                    foreach (var num in commonNumbers)
+                    {
+                        result += num.Value + "<br>";
+                    }
+                    lblResult.Text = "<b>Result: </b>" + "<br>" + result;
+                    lblFileUploaded.Visible = false;
+                }             
+                
             }
             else
             {
                 lblFileUploaded.Text = "No File Uploaded.";
-            }
-
-            if (fileUpLoader.HasFile)
-            {
-                StreamReader reader = new StreamReader(fileUpLoader.FileContent);
-                string content = reader.ReadToEnd().Trim();
-                string[] read = content.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-                List<Int64> lstNumbers = read.Select(Int64.Parse).ToList();
-                var commonNumbers = lstNumbers.GroupBy(m => m).Select(g => new { Value = g.Key, Count = g.Count() }).OrderByDescending(m => m.Count).Take(5);
-                string result = string.Empty;
-                foreach (var num in commonNumbers)
-				{
-                    result += num.Value + "<br>";
-                }
-                lblResult.Text = "<b>Result: </b>"+ "<br>" + result;
-                
+                lblFileUploaded.ForeColor = System.Drawing.Color.Red;
             }
 
         }
